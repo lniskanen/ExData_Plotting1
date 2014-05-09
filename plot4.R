acf850f66f290f0b085f820d7bca78eba39e0b0e
Lauri Niskanen <lauri.niskanen@iki.fi>
2014-05-09 09:52:56 +0300

    Turning off legend borders in plot4.png

--- plot4.R
+++ plot4.R
@@ -36,7 +36,7 @@ with(dataSet,{
   with(dataSet, plot(dataSet$DateTime,dataSet$Sub_metering_1,type="l",col="black",ylab="Energy sub metering", xlab=" "))
   with(subset(dataSet), lines(dataSet$DateTime,dataSet$Sub_metering_2,type="l",col="red"))
   with(subset(dataSet), lines(dataSet$DateTime,dataSet$Sub_metering_3,type="l",col="blue"))
-  legend("topright", lty= c(1), col = c("black", "red","blue"), legend =c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+  legend("topright", box.lwd = 0,box.col = "white",lty= c(1), col = c("black", "red","blue"), legend =c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
   
   
   ## Plot 2,2
@@ -46,7 +46,34 @@ with(dataSet,{
 
 ## Copy screen device to PNG file
 if(file.exists(destFile)) file.remove(destFile)
-dev.copy(png, file=destFile,width=480,height=480)
+##dev.copy(png, file=destFile,width=480,height=480)
+png(destFile, width=480, height=480)
+
+## re-plot for png screen
+
+par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(1, 1, 1, 1))
+
+with(dataSet,{
+  
+  ## Plot 1,1
+  plot(dataSet$DateTime,dataSet$Global_active_power, type="l",xlab="Weekday-Hour", 
+       ylab="Global Active Power (kilowatts)")
+  
+  ## Plot 1,2
+  plot(dataSet$DateTime,dataSet$Voltage,type="l",ylab="Voltage",xlab="datetime",col="black")
+  
+  ## Plot 2,1
+  with(dataSet, plot(dataSet$DateTime,dataSet$Sub_metering_1,type="l",col="black",ylab="Energy sub metering", xlab=" "))
+  with(subset(dataSet), lines(dataSet$DateTime,dataSet$Sub_metering_2,type="l",col="red"))
+  with(subset(dataSet), lines(dataSet$DateTime,dataSet$Sub_metering_3,type="l",col="blue"))
+  legend("topright",box.lwd = 0,box.col = "white", lty= c(1), col = c("black", "red","blue"), legend =c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+  
+  
+  ## Plot 2,2
+  plot(dataSet$DateTime,dataSet$Global_reactive_power,bty="n",type="l",ylab="Global_reactive_power",xlab="datetime",col="black")
+  
+})
+
 
 ## Close the screen device
 dev.off()
